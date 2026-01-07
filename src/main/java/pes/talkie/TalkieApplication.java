dd49d0e2a636c2ea97ce22847fc2a2ddd9629344
Per Eystein <per.eystein@gmail.com>
2026-01-07 14:26:26 +0100

    Template from Spring Initializr

---
+++ src/main/java/pes/talkie/TalkieApplication.java
@@ -0,0 +1,13 @@
+package pes.talkie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pes/talkie/TalkieApplicationTests.java
@@ -0,0 +1,13 @@
+package pes.talkie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
